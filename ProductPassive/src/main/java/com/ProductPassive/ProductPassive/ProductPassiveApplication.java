c76f0cdef062734caf30080a71f5540292f59d07
Luis Acuña <luisandresverdugox@gmail.com>
2022-11-25 19:17:32 -0500

    Subiendo código de Prodducto Pasivo

---
+++ ProductPassive/src/main/java/com/ProductPassive/ProductPassive/ProductPassiveApplication.java
@@ -0,0 +1,13 @@
+package com.ProductPassive.ProductPassive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPassiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPassiveApplication.class, args);
+	}
+
+}
